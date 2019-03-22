ee8a7c0af5644b64d324514582fa11dc26218867
Pegah <pegah@apeunit.com>
2019-03-22 12:08:36 +0100

    Deployed fded283 with MkDocs version: 1.0.4

--- docs/æternity/.circleci/split_suites.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-ls -1 apps/*/test/*SUITE.erl test/*SUITE.erl > suites.txt
-xargs -I {} basename {} .erl < suites.txt > classes.txt
-circleci tests split --split-by=timings --timings-type=classname classes.txt > batch.txt
-grep -f batch.txt suites.txt | paste -s -d ',' -
