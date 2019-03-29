a835daa1593cb0e71608f35d69356d2133b98225
Pegah <pegah@apeunit.com>
2019-03-29 18:16:56 +0100

    fixed relative path bug for 404 error page

---
+++ site/æternity/apps/aecore/src/aec_accounts.erl
@@ -0,0 +1,111 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2017, Aeternity Anstalt
+%%% @doc Accounts.
+%%% @end
+%%%-------------------------------------------------------------------
+-module(aec_accounts).
+
+%% API
+-export([new/2,
+         id/1,
+         pubkey/1,
+         balance/1,
+         nonce/1,
+         earn/2,
+         spend/3,
+         spend_without_nonce_bump/2,
+         set_nonce/2,
+         serialize/1,
+         deserialize/2,
+         serialize_for_client/1]).
+
+-define(ACCOUNT_VSN, 1).
+-define(ACCOUNT_TYPE, account).
+
+-record(account, {
+          id             :: aeser_id:id(),
+          balance = 0    :: non_neg_integer(),
+          nonce = 0      :: non_neg_integer()}).
+
+-opaque account() :: #account{}.
+-export_type([account/0, deterministic_account_binary_with_pubkey/0]).
+
+-type deterministic_account_binary_with_pubkey() :: binary().
+
+-spec new(aec_keys:pubkey(), non_neg_integer()) -> account().
+new(Pubkey, Balance) ->
+    Id = aeser_id:create(account, Pubkey),
+    #account{id = Id, balance = Balance}.
+
+-spec id(account()) -> aeser_id:id().
+id(#account{id = Id}) ->
+    Id.
+
+-spec pubkey(account()) -> aec_keys:pubkey().
+pubkey(#account{id = Id}) ->
+    aeser_id:specialize(Id, account).
+
+-spec balance(account()) -> non_neg_integer().
+balance(#account{balance = Balance}) ->
+    Balance.
+
+-spec nonce(account()) -> non_neg_integer().
+nonce(#account{nonce = Nonce}) ->
+    Nonce.
+
+%% Only used for tests
+-spec set_nonce(account(), non_neg_integer()) -> account().
+set_nonce(Account, NewNonce) ->
+    Account#account{nonce = NewNonce}.
+
+-spec earn(account(), non_neg_integer()) -> {ok, account()}.
+earn(#account{balance = Balance0} = Account0, Amount) ->
+    {ok, Account0#account{balance = Balance0 + Amount}}.
+
+-spec spend(account(), non_neg_integer(), non_neg_integer()) -> {ok, account()}.
+spend(#account{balance = Balance0} = Account0, Amount, Nonce) ->
+    {ok, Account0#account{balance = Balance0 - Amount,
+                          nonce = Nonce}}.
+
+-spec spend_without_nonce_bump(account(), non_neg_integer()) -> {ok, account()}.
+%%% NOTE: Only use this if you actually don't want to update the nonce
+%%% of the account (e.g., when opening a state channel).
+spend_without_nonce_bump(#account{balance = Balance0} = Account0, Amount) ->
+    {ok, Account0#account{balance = Balance0 - Amount}}.
+
+-spec serialize(account()) -> deterministic_account_binary_with_pubkey().
+serialize(Account) ->
+    aeser_chain_objects:serialize(
+      ?ACCOUNT_TYPE, ?ACCOUNT_VSN,
+      serialization_template(?ACCOUNT_VSN),
+      [ {nonce, nonce(Account)}
+      , {balance, balance(Account)}
+      ]).
+
+-spec deserialize(aec_keys:pubkey(), binary()) -> account().
+deserialize(Pubkey, SerializedAccount) ->
+    [ {nonce, Nonce}
+    , {balance, Balance}
+    ] = aeser_chain_objects:deserialize(
+          ?ACCOUNT_TYPE,
+          ?ACCOUNT_VSN,
+          serialization_template(?ACCOUNT_VSN),
+          SerializedAccount),
+    #account{ id = aeser_id:create(account, Pubkey)
+            , balance = Balance
+            , nonce = Nonce
+            }.
+
+serialization_template(?ACCOUNT_VSN) ->
+    [ {nonce, int}
+    , {balance, int}
+    ].
+
+-spec serialize_for_client(account()) -> map().
+serialize_for_client(#account{id      = Id,
+                              balance = Balance,
+                              nonce   = Nonce}) ->
+    #{<<"id">>      => aeser_api_encoder:encode(id_hash, Id),
+      <<"balance">> => Balance,
+      <<"nonce">>   => Nonce}.
+
---
+++ site/æternity/apps/aecore/src/aec_accounts_trees.erl
@@ -0,0 +1,194 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2017, Aeternity Anstalt
+%%% @doc Merkle trees of accounts.
+%%% @end
+%%%-------------------------------------------------------------------
+-module(aec_accounts_trees).
+
+%% API - similar to OTP `gb_trees` module
+-export([empty/0,
+         empty_with_backend/0,
+         get/2,
+         lookup/2,
+         new_with_backend/1,
+         gc_cache/1,
+         enter/2]).
+
+%% API - Merkle tree
+-export([root_hash/1,
+         commit_to_db/1,
+         mtree_iterator/1
+        ]).
+
+-export([ from_binary_without_backend/1
+        , to_binary_without_backend/1
+        ]).
+
+%% API - Proof of inclusion
+-export([ add_poi/3
+        , verify_poi/3
+        , lookup_poi/2
+        ]).
+
+-ifdef(TEST).
+-export([delete/2]).
+-endif.
+
+
+%% API - misc
+-export([ get_all_accounts_balances/1
+        , lock_coins/2]).
+
+-export_type([tree/0]).
+
+-type key() :: aec_keys:pubkey().
+-type value() :: aec_accounts:deterministic_account_binary_with_pubkey().
+-opaque tree() :: aeu_mtrees:mtree(key(), value()).
+
+-define(VSN, 1).
+%%%===================================================================
+%%% API - similar to OTP `gb_trees` module
+%%%===================================================================
+-spec empty() -> tree().
+empty() ->
+    aeu_mtrees:empty().
+
+-spec empty_with_backend() -> tree().
+empty_with_backend() ->
+    aeu_mtrees:empty_with_backend(aec_db_backends:accounts_backend()).
+
+-spec new_with_backend(aeu_mtrees:root_hash() | 'empty') -> tree().
+new_with_backend(Hash) ->
+    aeu_mtrees:new_with_backend(Hash, aec_db_backends:accounts_backend()).
+
+-spec gc_cache(tree()) -> tree().
+gc_cache(Tree) ->
+    aeu_mtrees:gc_cache(Tree).
+
+-spec get(aec_keys:pubkey(), tree()) -> aec_accounts:account().
+get(Pubkey, Tree) ->
+    Account = aec_accounts:deserialize(Pubkey, aeu_mtrees:get(Pubkey, Tree)),
+    Pubkey  = aec_accounts:pubkey(Account), %% Hardcoded expectation.
+    Account.
+
+-spec lookup(aec_keys:pubkey(), tree()) -> none | {value, aec_accounts:account()}.
+lookup(Pubkey, Tree) ->
+    case aeu_mtrees:lookup(Pubkey, Tree) of
+        none ->
+            none;
+        {value, SerializedAccount} ->
+            Account = aec_accounts:deserialize(Pubkey, SerializedAccount),
+            Pubkey  = aec_accounts:pubkey(Account), %% Hardcoded expectation.
+            {value, Account}
+    end.
+
+-spec enter(aec_accounts:account(), tree()) -> tree().
+enter(Account, Tree) ->
+    aeu_mtrees:enter(key(Account), value(Account), Tree).
+
+-ifdef(TEST).
+-spec delete(aec_keys:pubkey(), tree()) -> tree().
+delete(Pubkey, Tree) ->
+    aeu_mtrees:delete(Pubkey, Tree).
+-endif.
+
+-spec to_binary_without_backend(tree()) -> binary().
+to_binary_without_backend(Tree) ->
+    Bin = aeu_mtrees:serialize(Tree),
+    aeser_chain_objects:serialize(
+        accounts_mtree,
+        ?VSN,
+        serialization_template(?VSN),
+        [{accounts, Bin}]).
+
+-spec from_binary_without_backend(binary()) -> tree().
+from_binary_without_backend(Bin) ->
+    [{accounts, AccountsBin}] =
+        aeser_chain_objects:deserialize(accounts_mtree, ?VSN,
+                                             serialization_template(?VSN), Bin),
+    aeu_mtrees:deserialize(AccountsBin).
+
+serialization_template(?VSN) ->
+    [{accounts, binary}].
+
+-spec mtree_iterator(tree()) -> aeu_mtrees:iterator().
+mtree_iterator(Tree) ->
+    aeu_mtrees:iterator(Tree).
+
+%%%===================================================================
+%%% API - Merkle tree
+%%%===================================================================
+
+-spec root_hash(tree()) -> {ok, aeu_mtrees:root_hash()} | {error, empty}.
+root_hash(Tree) ->
+    aeu_mtrees:root_hash(Tree).
+
+-spec add_poi(aec_keys:pubkey(), tree(), aec_poi:poi()) ->
+                     {'ok', aec_poi:poi()}
+                         | {'error', 'not_present' | 'wrong_root_hash'}.
+add_poi(Pubkey, Tree, Poi) ->
+    aec_poi:add_poi(Pubkey, Tree, Poi).
+
+-spec verify_poi(aec_keys:pubkey(), aec_accounts:account(), aec_poi:poi()) ->
+                        'ok' | {'error', term()}.
+verify_poi(AccountKey, Account, Poi) ->
+    %% Hardcode expectation on specified account object key being
+    %% equal to key in internal representation of account.  The key is
+    %% not part of the account serialization so this shall never
+    %% happen.
+    AccountKey = aec_accounts:pubkey(Account),
+    aec_poi:verify(AccountKey, aec_accounts:serialize(Account), Poi).
+
+-spec lookup_poi(aec_keys:pubkey(), aec_poi:poi()) ->
+                        {'ok', aec_accounts:account()} | {'error', not_found}.
+lookup_poi(AccountKey, Poi) ->
+    case aec_poi:lookup(AccountKey, Poi) of
+        {ok, SerializedAccount} -> {ok, aec_accounts:deserialize(AccountKey, SerializedAccount)};
+        Err -> Err
+    end.
+
+-spec commit_to_db(tree()) -> tree().
+commit_to_db(Tree) ->
+    aeu_mtrees:commit_to_db(Tree).
+
+%%%===================================================================
+%%% API - misc
+%%%===================================================================
+
+-spec get_all_accounts_balances(tree()) -> [{aec_keys:pubkey(), non_neg_integer()}].
+get_all_accounts_balances(AccountsTree) ->
+    AccountsDump = aeu_mtrees:to_list(AccountsTree),
+    lists:foldl(
+      fun({Pubkey, SerializedAccount}, Acc) ->
+              Account = aec_accounts:deserialize(Pubkey, SerializedAccount),
+              [{Pubkey, aec_accounts:balance(Account)} | Acc]
+      end, [], AccountsDump).
+
+-spec lock_coins(integer(), tree()) -> tree().
+lock_coins(0     , AccountsTree) -> AccountsTree;
+lock_coins(Amount, AccountsTree) ->
+    HolderPubKey = aec_governance:locked_coins_holder_account(),
+    HolderAccount0 = case lookup(HolderPubKey, AccountsTree) of
+                        none             -> aec_accounts:new(HolderPubKey, 0);
+                        {value, Account} -> Account
+                    end,
+    {ok, HolderAccount} =
+        case Amount > 0 of
+            true -> aec_accounts:earn(HolderAccount0, Amount);
+            false ->
+                AbsAmount = abs(Amount),
+                true = 0 =< aec_accounts:balance(HolderAccount0) - AbsAmount,
+                Nonce = aec_accounts:nonce(HolderAccount0),
+                aec_accounts:spend(HolderAccount0, AbsAmount, Nonce)
+        end,
+    enter(HolderAccount, AccountsTree).
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+key(A) ->
+    aec_accounts:pubkey(A).
+
+value(A) ->
+    aec_accounts:serialize(A).
