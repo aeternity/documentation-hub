b3d1d57a2b7a3d6774e5996af950846d4f7d3f05
Pegah <pegah@apeunit.com>
2019-04-23 11:23:32 +0100

    Deployed dfaf620 with MkDocs version: 1.0.4

--- docs/developer-tools/frameworks/README.md
+++
@@ -1,3 +0,0 @@
-# Overview of Frameworks
-
-- [forgAE](forgae/README.md)
--- site/developer-tools/middleware/aepp-middleware/migrations/2019-02-26-113638_channel_identifiers/down.sql
+++
@@ -1,4 +0,0 @@
--- This file should undo anything in `up.sql`
-
-DROP TABLE IF EXISTS channel_identifiers;
--- This file should undo anything in `up.sql`
