fe9b3b3e1e9a8bb1c1b06760f3215edae2d862f0
Pegah <pegah@apeunit.com>
2019-04-25 16:50:26 +0200

    added submodule base aepp

---
+++ site/aepp-base/src/components/AeQrCode.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    :style="{
+      width: `${size}px`,
+      height: `${size}px`,
+    }"
+    class="ae-qr-code"
+  />
+</template>
+
+<script>
+import renderQrCodeSvg from '../lib/renderQrCodeSvg';
+
+export default {
+  props: {
+    data: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Number,
+      default: 170,
+    },
+  },
+  watch: {
+    data() {
+      this.renderQrCode();
+    },
+  },
+  mounted() {
+    this.renderQrCode();
+  },
+  methods: {
+    renderQrCode() {
+      if (this.$el.firstChild) this.$el.removeChild(this.$el.firstChild);
+      this.$el.appendChild(renderQrCodeSvg(this.data, this.size));
+    },
+  },
+};
+</script>
---
+++ site/aepp-base/src/store/plugins/registerServiceWorker.js
@@ -0,0 +1,23 @@
+import { register } from 'register-service-worker';
+
+export default (store) => {
+  if (process.env.NODE_ENV === 'production') {
+    register(`${process.env.BASE_URL}service-worker.js`, {
+      registered(registration) {
+        store.commit('setServiceWorkerRegistration', registration);
+      },
+      updated() {
+        store.dispatch('setNotification', {
+          text: 'New version is available, please restart the Base æpp',
+          autoClose: true,
+        });
+      },
+      offline() {
+        store.dispatch('setNotification', {
+          text: 'You are currently offline',
+          autoClose: true,
+        });
+      },
+    });
+  }
+};
